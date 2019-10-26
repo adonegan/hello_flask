f19149f4fa56f377fcf031d1791894b900800402
adonegan <48014273+adonegan@users.noreply.github.com>
2019-10-26 20:15:26 +0100

    added flash message

--- app.py
+++ app.py
@@ -31,7 +31,9 @@ def about_member(member_name):
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form["name"])
+        flash("Thanks {}, we have received your message".format(
+            request.form["name"]
+        ))
     return render_template("contact.html", page_title="Contact")
 
 @app.route('/careers')
